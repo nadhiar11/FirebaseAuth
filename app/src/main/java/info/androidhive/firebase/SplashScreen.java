bdbadcff4c4660059e866807f439e3c85de8278d
Nadhiar Ridho Wahyu Pradana <nadhiar11@gmail.com>
2016-12-18 14:20:21 +0700

    first commit

---
+++ app/src/main/java/info/androidhive/firebase/SplashScreen.java
@@ -0,0 +1,43 @@
+package info.androidhive.firebase;
+
+/**
+ * Created by 1312100999 on 12/10/2016.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by vamsikrishna on 12-Feb-15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(4000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
